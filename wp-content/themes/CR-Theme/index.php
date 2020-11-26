5e45bed72f638e16ddaddcfd6f6515f2314fbeff
mackilroy007 <mackilroy007@gmail.com>
2020-11-26 15:21:14 +0100

    adding unique theme folder structure

---
+++ wp-content/themes/CR-Theme/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta charset="<?php bloginfo('charset'); ?>"><!--displays the encoding-->
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+   <meta name="description" content="<?php bloginfo('description'); ?>"><!--displays the Tagline in Settings->General -->
+   <title><?php bloginfo('name'); ?></title>
+   <!-- Custom styles for this template -->
+   <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet"> <!--displays the primary CSS-->
+ </head>
+<body>
+
+   <h1>Hello from my theme</h1>
+
+<!-- All dynamic content will come in here -->
+
+</body>
+</html>
