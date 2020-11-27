6cbf634f4b97bc5717575fa61564ba92d949dc3e
mackilroy007 <mackilroy007@gmail.com>
2020-11-27 14:22:12 +0100

    style for the single post page

--- wp-content/themes/CR-Theme/single.php
+++ wp-content/themes/CR-Theme/single.php
@@ -1,20 +1,17 @@
 <?php get_header(); ?>
 
+<div class="container">
+    <h1>Hello from a single post</h1>
 
-<div class="content">
-
-    <h1>Hello from my theme</h1>
 
     <?php if (have_posts()) : ?>
         <!--  If there are posts available  -->
 
         <?php while (have_posts()) : the_post(); ?>
             <!-- if there are posts, iterate the posts in the loop-->
-            <a href="<?php the_permalink(); ?>">
-                <!--retrieves URL for the permalink-->
-                <?php the_title(); ?>
-                <!--retrieves blog title-->
-            </a>
+
+            <?php the_title(); ?>
+            <!--retrieves blog title-->
 
             <p><?php the_time('F j, Y g:i a'); ?></p>
             <!--retrieves date blog entry was created-->
